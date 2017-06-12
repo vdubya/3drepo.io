0495627816c52a3c4c3d639c9bfd7fbd479771c0
Henry Liu <thenrikie@hotmail.com>
2017-06-12 16:44:09 +0100

    ISSUE #373 - db migration script to rename filename in stash collection

---
+++ scripts/db_migrations/v2/renameFilename.js
@@ -0,0 +1,27 @@
+print('Renaming filename in stash collection');
+
+db.getSiblingDB('admin').adminCommand({listDatabases:1}).databases.forEach(function(database){
+	var myDb = db.getSiblingDB(database.name);
+	myDb.getCollectionNames().forEach(function(collection){
+
+		if((/^\w+\.stash\.\w+\.files$/).test(collection)){
+			var myCollection = myDb[collection];
+			print('processing ' + database.name + ' ' + collection + '....');
+			myCollection.find().forEach(function(item){
+				if(item.filename){
+					var urls = item.filename.split('/');
+					if(urls.length === 4){
+						urls.splice(1,0,'teamspaces');
+						urls.splice(3,0,'models');
+						myCollection.update({ _id : item._id }, { '$set': { filename: urls.join('/')} });
+					}
+				}
+			});
+
+		}
+
+		
+	});
+});
+
+print('Done');
