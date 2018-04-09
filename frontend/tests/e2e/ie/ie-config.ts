80574b56adab4883c0c325222fbf8106b60bb2d3
James Milner <jamesmilner22@hotmail.com>
2018-04-09 16:27:23 +0100

    ISSUE #867 - Try downgrading in Chrome

--- frontend/tests/e2e/ie/ie-config.ts
+++ frontend/tests/e2e/ie/ie-config.ts
@@ -9,21 +9,21 @@ export let config: Config = {
 	slow: 3000,
 	},
 	capabilities: {
-	"browserName": "firefox",
-		"moz:firefoxOptions": {
-		args : [
+		// "browserName": "firefox",
+		// 	"moz:firefoxOptions": {
+		// 	args : [
+		// 		"--headless",
+		// 	],
+		// },
+		browserName: "chrome",
+		chromeOptions: {
+			args: [
 			"--headless",
-		],
-	},
-	// 'browserName': 'chrome',
-	// chromeOptions: {
-	//   args: [
-	//     "--headless",
-	//     "--disable-gpu",
-	//     "--window-size=1920x1080",
-	//     '--user-agent="Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko'
-	//   ]
+			// "--disable-gpu",
+			"--window-size=1920x1080",
+			'--user-agent="Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko',
+			],
 
-	// }
+		},
 	},
 };
